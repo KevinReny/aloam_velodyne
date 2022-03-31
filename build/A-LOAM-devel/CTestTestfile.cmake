8b0c7fda6d5c9f56e52cc091091f81dd3e73f504
KevinReny <1678202161@qq.com>
2022-03-31 16:36:43 +0800

    Initial commit

---
+++ build/A-LOAM-devel/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ren/catkin_ws/catkin_aloam/src/A-LOAM-devel
+# Build directory: /home/ren/catkin_ws/catkin_aloam/build/A-LOAM-devel
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/a-loam-noted-master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ren/catkin_ws/catkin_aloam/src/a-loam-noted-master
+# Build directory: /home/ren/catkin_ws/catkin_aloam/build/a-loam-noted-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
